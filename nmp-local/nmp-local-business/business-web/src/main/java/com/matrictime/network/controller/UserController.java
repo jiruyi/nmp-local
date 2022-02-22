72a08fb934eaeebbf521f5ea1d2c0bd69f5b3ba6
jiruyi <guanzididi@163.com>
2022-02-22 16:55:00 +0800

    jiruyi  框架搭建

---
+++ nmp-local/nmp-local-business/business-web/src/main/java/com/matrictime/network/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.matrictime.network.controller;
+
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author jiruyi
+ * @copyright www.matrictime.com
+ * @project nmp-local
+ * @date 2022/2/21 0021 17:38
+ * @desc
+ */
+@RequestMapping(value = "/user")
+@Api(value = "用户信息相关",tags = "用户信息相关")
+@RestController
+@Slf4j
+public class UserController {
+
+
+
+
+}
