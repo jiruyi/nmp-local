1eefdecbc37f6495dc0968611abf31669587cf8f
zhangyunjie <836507105@qq.com>
2022-04-11 09:50:14 +0800

    用户中心用户组模块

--- nmp-local/user-center/src/main/java/com/matrictime/network/controller/GroupController.java
+++ nmp-local/user-center/src/main/java/com/matrictime/network/controller/GroupController.java
@@ -1,10 +1,12 @@
 package com.matrictime.network.controller;
 
 import com.matrictime.network.api.request.GroupReq;
+import com.matrictime.network.api.request.UserGroupReq;
 import com.matrictime.network.api.request.UserRequest;
 import com.matrictime.network.exception.ErrorMessageContants;
 import com.matrictime.network.model.Result;
 import com.matrictime.network.service.GroupService;
+import com.matrictime.network.service.UserGroupService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
@@ -23,6 +25,9 @@ public class GroupController {
 
     @Autowired
     GroupService groupService;
+    @Autowired
+    UserGroupService userGroupService;
+
 
     @ApiOperation(value = "创建用户组",notes = "创建")
     @RequestMapping (value = "/createGroup",method = RequestMethod.POST)
@@ -46,6 +51,32 @@ public class GroupController {
     @RequestMapping (value = "/queryGroup",method = RequestMethod.POST)
     public Result queryGroup(@RequestBody GroupReq groupReq){
         return groupService.queryGroup(groupReq);
+    }
+
+    //-----------------------------------------------------------------------
+
+    @ApiOperation(value = "创建用户组",notes = "创建")
+    @RequestMapping (value = "/createUserGroup",method = RequestMethod.POST)
+    public Result createUserGroup(@RequestBody UserGroupReq userGroupReq){
+        return userGroupService.createUserGroup(userGroupReq);
+    }
+
+    @ApiOperation(value = "删除用户组",notes = "删除")
+    @RequestMapping (value = "/deleteUserGroup",method = RequestMethod.POST)
+    public Result deleteUserGroup(@RequestBody UserGroupReq userGroupReq){
+        return userGroupService.deleteUserGroup(userGroupReq);
+    }
+
+    @ApiOperation(value = "修改用户组",notes = "修改")
+    @RequestMapping (value = "/modifyUserGroup",method = RequestMethod.POST)
+    public Result modifyUserGroup(@RequestBody UserGroupReq userGroupReq){
+        return userGroupService.modifyUserGroup(userGroupReq);
+    }
+
+    @ApiOperation(value = "查询用户组",notes = "查询")
+    @RequestMapping (value = "/queryUserGroup",method = RequestMethod.POST)
+    public Result queryUserGroup(@RequestBody UserGroupReq userGroupReq){
+        return userGroupService.queryUserGroup(userGroupReq);
     }
 
 
