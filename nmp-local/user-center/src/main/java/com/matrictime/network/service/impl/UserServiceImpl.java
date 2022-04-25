444d5c63b4c5725118d7bed2bce8c1fd0b165ca5
zhangyunjie <836507105@qq.com>
2022-04-25 15:53:06 +0800

    接口测试调整

--- nmp-local/user-center/src/main/java/com/matrictime/network/service/impl/UserServiceImpl.java
+++ nmp-local/user-center/src/main/java/com/matrictime/network/service/impl/UserServiceImpl.java
@@ -309,7 +309,10 @@ public class UserServiceImpl   extends SystemBaseService implements UserService 
             throw new SystemException("无此用户");
         }
         UserVo userVo =new UserVo();
-        BeanUtils.copyProperties(user,userVo);
+        userVo.setUserId(user.getUserId());
+        userVo.setPhoneNumber(user.getPhoneNumber());
+        userVo.setNickName(user.getNickName());
+//        BeanUtils.copyProperties(user,userVo);
         return buildResult(userVo);
     }
 
@@ -321,7 +324,7 @@ public class UserServiceImpl   extends SystemBaseService implements UserService 
                 ObjectUtils.isEmpty(changePasswdReq.getPhoneNumber())){
             return new Result(false, ErrorMessageContants.PARAM_IS_NULL_MSG);
         }
-        if(changePasswdReq.getRepeatPassword().equals(changePasswdReq.getNewPassword())){
+        if(!changePasswdReq.getRepeatPassword().equals(changePasswdReq.getNewPassword())){
             throw new SystemException("两次输入密码不一致");
         }
 
