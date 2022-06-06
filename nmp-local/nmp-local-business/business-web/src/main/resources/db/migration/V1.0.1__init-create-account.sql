9e80846987ce77c71bb5f05f5c4bf45aba2c6801
zhangyunjie <836507105@qq.com>
2022-06-06 09:42:48 +0800

    菜单查询改成查看

--- nmp-local/nmp-local-business/business-web/src/main/resources/db/migration/V1.0.1__init-create-account.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `account` (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-    `userid` int(11) DEFAULT NULL,
-    `money` double DEFAULT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
