b201c1c74b04956957c660e2b0504b4bee498ea2
hexu <hexu@jmail.com>
2022-06-27 15:26:29 +0800

    [优化]新增redis配置和单点登录部分代码

---
+++ nmp-local/user-center/src/main/java/com/matrictime/network/controller/aop/WebConfig.java
@@ -0,0 +1,54 @@
+package com.matrictime.network.controller.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author jiruyi
+ * @copyright www.matrictime.com
+ * @project microservicecloud-jzsg
+ * @date 2021/9/7 0007 14:48
+ * @desc
+ */
+@Slf4j
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private SessionHandlerInterceptor sessionHandlerInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(sessionHandlerInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/**/user/login/**")
+                .excludePathPatterns("/**/health/**")
+                .excludePathPatterns("/**/log/device/save/**")
+                .excludePathPatterns("/**/device/supplementDeviceInfo/**")
+                .excludePathPatterns("/**/node/getAdjoinNode/**")
+                .excludePathPatterns("/**/node/getNode/**")
+                .excludePathPatterns("/**/route/query/**")
+                .excludePathPatterns("/**/keyDetail/insertKey/**")
+                .excludePathPatterns("/**/beans/**")
+                .excludePathPatterns("/**/error/**")
+                .excludePathPatterns("/**/swagger-resources/**")
+                .excludePathPatterns("/**/v3/**")
+                .excludePathPatterns("/**/swagger-ui/**")
+                .excludePathPatterns("/**/sms/send")
+                .excludePathPatterns("/**/device/selectDevice/**")
+                .excludePathPatterns("/**/druid/**")
+                .excludePathPatterns("/**/data/saveData/**")
+                .excludePathPatterns("/**/bill/saveBill/**")
+                .excludePathPatterns("/signal/addSignal")
+                .excludePathPatterns("/monitor/checkHeart")
+                .excludePathPatterns("/version/getVersionFile")
+                .excludePathPatterns("/linkRelation/selectDevice")
+                .excludePathPatterns("/baseStation/selectDevice")
+                .excludePathPatterns("/baseStation/selectByOperatorId")
+                .excludePathPatterns("/outlineSorter/auth")
+                .excludePathPatterns("/config/insertOrUpdate");
+    }
+}
