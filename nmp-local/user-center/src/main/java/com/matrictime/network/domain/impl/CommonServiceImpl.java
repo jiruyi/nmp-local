132d2222cd7b34e44e9de7b3afb0649bff800f91
hexu <hexu@jmail.com>
2022-07-08 15:41:43 +0800

    [优化]修复推送bug

--- nmp-local/user-center/src/main/java/com/matrictime/network/domain/impl/CommonServiceImpl.java
+++ nmp-local/user-center/src/main/java/com/matrictime/network/domain/impl/CommonServiceImpl.java
@@ -70,18 +70,28 @@ public class CommonServiceImpl extends SystemBaseService implements CommonServic
             }
             ReqUtil resUtil = new ReqUtil();
             log.info("encryptForWs errormsg:{}",res.getErrorMsg());
-            WsResultVo wsResultVo = JSONObject.parseObject(res.getErrorMsg(), new TypeReference<WsResultVo>() {});
-            String result = wsResultVo.getResult();
-            wsResultVo.setDestination(UcConstants.DESTINATION_IN);
-            String encryJsonToReq = resUtil.encryJsonToReq(result, getSidByCondition(wsResultVo.getSendObject()));
-            wsResultVo.setResult(encryJsonToReq);
-            res.setErrorMsg(null);
-            String sid = getSidByCondition(condition);
-            String resultObj = resUtil.encryJsonToReq(res, sid);
-            res.setSuccess(true);
-            res.setResultObj(resultObj);
-            res.setErrorMsg(JSONObject.toJSONString(wsResultVo));
-            res.setErrorCode(null);
+            if (res.isSuccess()){
+                WsResultVo wsResultVo = JSONObject.parseObject(res.getErrorMsg(), new TypeReference<WsResultVo>() {});
+                String result = wsResultVo.getResult();
+                wsResultVo.setDestination(UcConstants.DESTINATION_IN);
+                String encryJsonToReq = resUtil.encryJsonToReq(result, getSidByCondition(wsResultVo.getSendObject()));
+                wsResultVo.setResult(encryJsonToReq);
+                res.setErrorMsg(null);
+                String sid = getSidByCondition(condition);
+                String resultObj = resUtil.encryJsonToReq(res, sid);
+                res.setSuccess(true);
+                res.setResultObj(resultObj);
+                res.setErrorMsg(JSONObject.toJSONString(wsResultVo));
+                res.setErrorCode(null);
+            }else {
+                res.setErrorMsg(null);
+                String sid = getSidByCondition(condition);
+                String resultObj = resUtil.encryJsonToReq(res, sid);
+                res.setSuccess(true);
+                res.setResultObj(resultObj);
+                res.setErrorCode(null);
+            }
+
         }
         return res;
     }
