78a4711578c461d42078352550a3bd2a088a0474
zhangyunjie <836507105@qq.com>
2022-11-18 10:29:27 +0800

    [记录代理推送的错误或者异常日志]

--- nmp-local/nmp-local-proxy/proxy-web/src/main/resources/db/migration/init-create-nmpl_proxy.sql
+++ nmp-local/nmp-local-proxy/proxy-web/src/main/resources/db/migration/init-create-nmpl_proxy.sql
@@ -354,6 +354,15 @@ CREATE TABLE IF NOT EXISTS `nmpl_bill` (
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='话单上报表';
 
+CREATE TABLE `nmpl_error_push_log` (
+                                       `id` bigint NOT NULL AUTO_INCREMENT COMMENT '自增主键',
+                                       `url` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '推送url',
+                                       `data` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '推送信息',
+                                       `error_msg` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '异常信息',
+                                       PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='代理异常推送日志';
+
+
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
