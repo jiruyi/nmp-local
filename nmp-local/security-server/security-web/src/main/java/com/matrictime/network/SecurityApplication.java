685d0cc9beb1ed2a3d6dc6b4edcc92404e34bc45
zhangyunjie <836507105@qq.com>
2022-12-20 14:01:46 +0800

    [安全服务器框架搭建]

---
+++ nmp-local/security-server/security-web/src/main/java/com/matrictime/network/SecurityApplication.java
@@ -0,0 +1,18 @@
+package com.matrictime.network;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@MapperScan(basePackages = "com.matrictime.network.dao.mapper")
+@EnableTransactionManagement
+public class SecurityApplication {
+    public static void main(String[] args)
+    {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(SecurityApplication.class, args);
+
+    }
+}
