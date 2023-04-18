bc2d7dfeec09b2809b228ad6774f7c2d1e620d95
zhangyunjie <836507105@qq.com>
2023-04-18 09:14:52 +0800

    [flyway ddl语句错误调整]

--- nmp-local/nmp-local-business/business-web/src/main/resources/db/migration/V3.0.18__init-data-report.sql
+++ nmp-local/nmp-local-business/business-web/src/main/resources/db/migration/V3.0.18__init-data-report.sql
@@ -1,5 +1,5 @@
 -- jry
-CREATE TABLE `nmpl_device_alarm` (
+CREATE TABLE IF NOT EXISTS `nmpl_device_alarm` (
     `alarm_id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键',
     `alarm_source_id` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '业务系统id  物理设备无',
     `alarm_source_ip` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '设备ip',
@@ -13,7 +13,7 @@ CREATE TABLE `nmpl_device_alarm` (
     PRIMARY KEY (`alarm_id`),
     KEY `idx_device_id` (`alarm_source_id`),
     KEY `upload_time_key` (`alarm_upload_time`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- jry
 
@@ -43,12 +43,12 @@ CREATE TABLE IF NOT EXISTS `nmpl_physical_device_resource` (
 
 
 CREATE TABLE IF NOT EXISTS `nmpl_system_resource` (
-    `system_id`  varchar(64) NOT NULL COMMNET '主键（系统id，关联基站和设备表）',
-    `system_type` varchar(50) DEFAULT NULL COMMNET '系统类别',
-    `start_time` datetime DEFAULT NULL COMMNET '启动时间',
-    `run_time` bigint DEFAULT 0 COMMNET '运行时长，以秒为单位',
-    `cpu_percent` varchar(8) DEFAULT '0' COMMNET 'CPU占比，百分比形式',
-    `memory_percent` varchar(8) DEFAULT '0' COMMNET '内存占比，百分比形式',
+    `system_id`  varchar(64) NOT NULL COMMENT '主键（系统id，关联基站和设备表）',
+    `system_type` varchar(50) DEFAULT NULL COMMENT '系统类别',
+    `start_time` datetime(2) DEFAULT NULL COMMENT '启动时间',
+    `run_time` bigint DEFAULT 0 COMMENT '运行时长，以秒为单位',
+    `cpu_percent` varchar(8) DEFAULT '0' COMMENT 'CPU占比，百分比形式',
+    `memory_percent` varchar(8) DEFAULT '0' COMMENT '内存占比，百分比形式',
     `upload_time` datetime(2) NOT NULL COMMENT '上报时间',
     `create_time` datetime(2) DEFAULT CURRENT_TIMESTAMP(2) COMMENT '创建时间',
     `update_time` datetime(2) DEFAULT CURRENT_TIMESTAMP(2) ON UPDATE CURRENT_TIMESTAMP(2) COMMENT '更新时间',
@@ -59,6 +59,22 @@ CREATE TABLE IF NOT EXISTS `nmpl_system_resource` (
 
 -- zyj
 
+DROP TABLE `nmpl_data_collect`;
+CREATE TABLE IF NOT EXISTS `nmpl_data_collect` (
+    `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键',
+    `device_id` varchar(32) DEFAULT NULL COMMENT '设备id',
+    `device_name` varchar(30) DEFAULT NULL COMMENT '设备名字',
+    `device_ip` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '设备ip',
+    `device_type` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '设备类别(01接入基站、02边界基站、11密钥中心、12生成机、13缓存机)',
+    `data_item_name` varchar(50) DEFAULT NULL COMMENT '统计项名',
+    `data_item_code` varchar(32) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL COMMENT '收集项编号',
+    `data_item_value` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT '1' COMMENT '值',
+    `unit` varchar(32) DEFAULT NULL COMMENT '单位',
+    `upload_time` datetime(2) NOT NULL COMMENT '创建时间',
+    `create_time` datetime(2) DEFAULT CURRENT_TIMESTAMP(2) COMMENT '创建时间',
+    `update_time` datetime(2) DEFAULT CURRENT_TIMESTAMP(2) ON UPDATE CURRENT_TIMESTAMP(2) COMMENT '更新时间',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3737590 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- zyj
 
