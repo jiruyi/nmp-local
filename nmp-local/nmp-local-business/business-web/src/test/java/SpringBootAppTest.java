31131724b31ecaa7dba086772323a27f7727a48a
jiruyi <guanzididi@163.com>
2023-05-09 11:10:54 +0800

    jiruyi   告警信息推送

--- nmp-local/nmp-local-business/business-web/src/test/java/SpringBootAppTest.java
+++ nmp-local/nmp-local-business/business-web/src/test/java/SpringBootAppTest.java
@@ -39,7 +39,7 @@ public class SpringBootAppTest {
         alarmInfoList.add(AlarmInfo.builder().alarmSourceIp("192.168.72.21").alarmUploadTime(new Date()).alarmContentType("1").alarmSourceType("00").alarmLevel("1").build());
         alarmInfoList.add(AlarmInfo.builder().alarmSourceIp("192.168.72.21").alarmUploadTime(new Date()).alarmContentType("2").alarmSourceType("00").alarmLevel("2").build());
         alarmInfoList.add(AlarmInfo.builder().alarmSourceIp("192.168.72.21").alarmUploadTime(new Date()).alarmContentType("3").alarmSourceType("00").alarmLevel("3").build());
-        alarmInfoList.add(AlarmInfo.builder().alarmSourceIp("192.168.72.21").alarmUploadTime(new Date()).alarmContentType("4").alarmSourceType("00").alarmLevel("4").build());
+        alarmInfoList.add(AlarmInfo.builder().alarmSourceIp("192.168.72.21").alarmUploadTime(new Date()).alarmContentType("4").alarmSourceType("00").alarmLevel("2").build());
         alarmInfoList.add(AlarmInfo.builder().alarmSourceIp("192.168.72.21").alarmUploadTime(new Date()).alarmContentType("1").alarmSourceType("00").alarmLevel("1").build());
         alarmInfoList.add(AlarmInfo.builder().alarmSourceIp("192.168.72.21").alarmUploadTime(new Date()).alarmContentType("1").alarmSourceType("00").alarmLevel("2").build());
         alarmInfoList.add(AlarmInfo.builder().alarmSourceIp("192.168.72.21").alarmUploadTime(new Date()).alarmContentType("2").alarmSourceType("00").alarmLevel("3").build());
@@ -48,6 +48,20 @@ public class SpringBootAppTest {
         alarmInfoList.add(AlarmInfo.builder().alarmSourceIp("192.168.72.21").alarmUploadTime(DateUtils.addDayForNow(-2)).alarmSourceType("00").alarmLevel("1").alarmContentType("2").alarmContent("内存不足").build());
         alarmInfoList.add(AlarmInfo.builder().alarmSourceIp("192.168.72.20").alarmUploadTime(DateUtils.addDayForNow(-1)).alarmSourceType("00").alarmLevel("2").alarmContentType("3").alarmContent("磁盘满载").build());
         alarmInfoList.add(AlarmInfo.builder().alarmSourceIp("192.168.72.20").alarmUploadTime(DateUtils.addDayForNow(-2)).alarmSourceType("00").alarmLevel("1").alarmContentType("4").alarmContent("流量过高").build());
+
+        alarmInfoList.add(AlarmInfo.builder().alarmSourceIp("192.168.72.103").alarmUploadTime(new Date()).alarmContentType("5").alarmSourceType("01").alarmLevel("1").build());
+        alarmInfoList.add(AlarmInfo.builder().alarmSourceIp("192.168.72.103").alarmUploadTime(new Date()).alarmContentType("5").alarmSourceType("01").alarmLevel("2").build());
+        alarmInfoList.add(AlarmInfo.builder().alarmSourceIp("192.168.72.103").alarmUploadTime(new Date()).alarmContentType("5").alarmSourceType("01").alarmLevel("3").build());
+        alarmInfoList.add(AlarmInfo.builder().alarmSourceIp("192.168.72.20").alarmUploadTime(new Date()).alarmContentType("5").alarmSourceType("01").alarmLevel("1").build());
+        alarmInfoList.add(AlarmInfo.builder().alarmSourceIp("192.168.72.24").alarmUploadTime(new Date()).alarmContentType("5").alarmSourceType("02").alarmLevel("2").build());
+        alarmInfoList.add(AlarmInfo.builder().alarmSourceIp("192.168.72.24").alarmUploadTime(new Date()).alarmContentType("5").alarmSourceType("02").alarmLevel("3").build());
+        alarmInfoList.add(AlarmInfo.builder().alarmSourceIp("192.168.72.24").alarmUploadTime(new Date()).alarmContentType("5").alarmSourceType("02").alarmLevel("1").build());
+        alarmInfoList.add(AlarmInfo.builder().alarmSourceIp("192.168.72.23").alarmUploadTime(new Date()).alarmContentType("5").alarmSourceType("11").alarmLevel("1").build());
+        alarmInfoList.add(AlarmInfo.builder().alarmSourceIp("192.168.72.23").alarmUploadTime(new Date()).alarmContentType("5").alarmSourceType("11").alarmLevel("2").build());
+        alarmInfoList.add(AlarmInfo.builder().alarmSourceIp("192.168.72.23").alarmUploadTime(new Date()).alarmContentType("5").alarmSourceType("11").alarmLevel("3").build());
+        alarmInfoList.add(AlarmInfo.builder().alarmSourceIp("192.168.72.23").alarmUploadTime(new Date()).alarmContentType("5").alarmSourceType("11").alarmLevel("3").build());
+        alarmInfoList.add(AlarmInfo.builder().alarmSourceIp("192.168.72.20").alarmUploadTime(DateUtils.addDayForNow(-1)).alarmSourceType("01").alarmLevel("2").alarmContentType("5").alarmContent("系统崩溃").build());
+        alarmInfoList.add(AlarmInfo.builder().alarmSourceIp("192.168.72.20").alarmUploadTime(DateUtils.addDayForNow(-2)).alarmSourceType("01").alarmLevel("1").alarmContentType("5").alarmContent("系统塌了").build());
         alarmDataDomainService.acceptAlarmData(alarmInfoList);
     }
 
