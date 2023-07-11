c7e75542dfbe58c706ff3834afe3bc31d3eff809
hexu <hexu@jmail.com>
2023-07-11 15:37:26 +0800

    设备枚举类

---
+++ nmp-local/nmp-local-common/src/main/java/com/matrictime/network/enums/DeviceTypeEnum.java
@@ -0,0 +1,29 @@
+package com.matrictime.network.enums;
+
+public enum DeviceTypeEnum {
+    STATION_BASE("00","基站"),
+    STATION_INSIDE("01","接入基站"),
+    STATION_BOUNDARY("02","边界基站"),
+
+    DEVICE_BASE("10","其他设备"),
+    DEVICE_DISPENSER("11","密钥中心"),
+    DEVICE_GENERATOR("12","生成机"),
+    DEVICE_CACHE("13","缓存机"),
+
+    DATA_BASE("20","数据采集");
+    private String code;
+    private String conditionDesc;
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getConditionDesc() {
+        return conditionDesc;
+    }
+
+    DeviceTypeEnum(String code, String conditionDesc) {
+        this.code = code;
+        this.conditionDesc = conditionDesc;
+    }
+}
