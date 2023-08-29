73c828813e213f0b4f31c6cc5364e12b8c64d561
jiruyi <guanzididi@163.com>
2023-08-29 15:48:48 +0800

    jiruyi   数据采集开发

---
+++ nmp-local/nmp-center-business/center-business-base/src/main/java/com/matrictime/network/base/util/SpringContextUtils.java
@@ -0,0 +1,86 @@
+package com.matrictime.network.base.util;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Map;
+
+/**
+ * @author jiruyi
+ * @copyright www.matrictime.com
+ * @project nmp-local
+ * @date 2023/8/29 0029 15:13
+ * @desc
+ */
+public class SpringContextUtils {
+
+    private static ApplicationContext applicationContext;
+
+    public static void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        SpringContextUtils.applicationContext = applicationContext;
+    }
+
+    /**
+     * 获取Spring的上下文
+     * @return
+     */
+    public static ApplicationContext getApplicationContext(){
+        return applicationContext;
+    }
+
+    /**
+     * 获取Spring上下文容器的Bean名称
+     * @return
+     */
+    public static String[] getBeanDefinitionNames(){
+        return applicationContext.getBeanDefinitionNames();
+    }
+
+    /**
+     * 根据Bean的名称获取Bean
+     * @param name Bean名称
+     * @param clazz 需要获取的实体
+     * @param <T>
+     * @return
+     */
+    public static <T> T getBean(String name,Class<T> clazz){
+        return applicationContext.getBean(name,clazz);
+    }
+
+    /**
+     * 根据Bean的名称获取Bean
+     * @param name
+     * @return
+     */
+    public static Object getBean(String name){
+        return applicationContext.getBean(name);
+    }
+
+    /**
+     * 根据class获取Bean
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    public static <T> T getBean(Class<T> clazz){
+        return applicationContext.getBean(clazz);
+    }
+
+    /**
+     * 根据Bean名称获取Class
+     * @param name
+     * @return
+     */
+    public static Class<?> getType(String name){
+        return applicationContext.getType(name);
+    }
+
+    /**
+     * 根据类型获取实现类
+     * @param classes
+     * @return
+     */
+    public static <T>  Map<String,T> getBeansOfType(Class<T> classes){
+        return applicationContext.getBeansOfType(classes);
+    }
+}
---
+++ nmp-local/nmp-center-business/center-business-service/src/main/java/com/matrictime/network/netty/server/NettyServerHandler.java
@@ -0,0 +1,74 @@
+package com.matrictime.network.netty.server;
+
+import com.alibaba.fastjson.JSONObject;
+import com.matrictime.network.base.util.SpringContextUtils;
+import com.matrictime.network.modelVo.DataPushBody;
+import com.matrictime.network.service.DataHandlerService;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.ObjectUtils;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.Map;
+
+/**
+ * @author jiruyi
+ * @copyright www.matrictime.com
+ * @project nmp-local
+ * @date 2023/8/29 0029 11:21
+ * @desc
+ */
+@Slf4j
+@ChannelHandler.Sharable
+public class NettyServerHandler extends SimpleChannelInboundHandler<byte[]> {
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, byte[] msg) throws Exception {
+        try {
+            if(ObjectUtils.isEmpty(msg)){
+                log.info("收到客户端的业务消息为空：{}",msg);
+            }
+            DataPushBody pushBody = parseMsg(msg);
+            handlerMapping(pushBody);
+        }catch (Exception e){
+            log.error("NettyServerHandler channelRead0 error:{}",e);
+        }
+    }
+
+    /**
+      * @title parseMsg
+      * @param [msg]
+      * @return com.matrictime.network.modelVo.DataPushBody
+      * @description  解析数据
+      * @author jiruyi
+      * @create 2023/8/29 0029 14:06
+      */
+    public DataPushBody parseMsg(byte[] msg){
+        ByteBuffer byteBuffer = ByteBuffer.allocate(msg.length);
+        byteBuffer.order(ByteOrder.BIG_ENDIAN);
+        byteBuffer.put(msg);
+        String reqDataJsonStr = new String(byteBuffer.array());
+        log.info("收到客户端的业务消息：{}",reqDataJsonStr);
+        return JSONObject.parseObject(reqDataJsonStr, DataPushBody.class);
+    }
+
+    /**
+      * @title handlerMapping
+      * @param [dataPushBody]
+      * @return void
+      * @description  业务分发
+      * @author jiruyi
+      * @create 2023/8/29 0029 15:34
+      */
+    public void handlerMapping(DataPushBody dataPushBody){
+        log.info("DataPushBody businessCode is:{},tableName is :{}"
+                ,dataPushBody.getBusinessCode(),dataPushBody.getTableName());
+        Map<String,DataHandlerService> map =
+        SpringContextUtils.getBeansOfType(DataHandlerService.class);
+        map.get(dataPushBody.getBusinessCode()).handlerData(dataPushBody);
+    }
+
+}
