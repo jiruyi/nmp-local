b418435d922533ed536fca6d30768e5f7c752c45
wangqiang <wq199269>
2023-11-09 16:17:40 +0800

    代理静态路由

---
+++ nmp-local/nmp-local-proxy/proxy-dao/src/main/java/com/matrictime/network/dao/model/NmplStaticRoute.java
@@ -0,0 +1,168 @@
+package com.matrictime.network.dao.model;
+
+import java.util.Date;
+import lombok.Data;
+
+/**
+ * 静态路由
+ * @author   wangqiang
+ * @date   2023-11-09
+ */
+@Data
+public class NmplStaticRoute {
+    /**
+     * 主键
+     */
+    private Long id;
+
+    /**
+     * 路由Id
+     */
+    private String routeId;
+
+    /**
+     * 小区id
+     */
+    private String companyId;
+
+    /**
+     * 基站id
+     */
+    private String stationId;
+
+    /**
+     * 入网Id
+     */
+    private String networkId;
+
+    /**
+     * 设备入网Id
+     */
+    private String deviceId;
+
+    /**
+     * 服务名称
+     */
+    private String serverName;
+
+    /**
+     * 删除标志（1代表存在 0代表删除）
+     */
+    private Boolean isExist;
+
+    /**
+     * 创建者
+     */
+    private String createUser;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 更新者
+     */
+    private String updateUser;
+
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRouteId() {
+        return routeId;
+    }
+
+    public void setRouteId(String routeId) {
+        this.routeId = routeId == null ? null : routeId.trim();
+    }
+
+    public String getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(String companyId) {
+        this.companyId = companyId == null ? null : companyId.trim();
+    }
+
+    public String getStationId() {
+        return stationId;
+    }
+
+    public void setStationId(String stationId) {
+        this.stationId = stationId == null ? null : stationId.trim();
+    }
+
+    public String getNetworkId() {
+        return networkId;
+    }
+
+    public void setNetworkId(String networkId) {
+        this.networkId = networkId == null ? null : networkId.trim();
+    }
+
+    public String getDeviceId() {
+        return deviceId;
+    }
+
+    public void setDeviceId(String deviceId) {
+        this.deviceId = deviceId == null ? null : deviceId.trim();
+    }
+
+    public String getServerName() {
+        return serverName;
+    }
+
+    public void setServerName(String serverName) {
+        this.serverName = serverName == null ? null : serverName.trim();
+    }
+
+    public Boolean getIsExist() {
+        return isExist;
+    }
+
+    public void setIsExist(Boolean isExist) {
+        this.isExist = isExist;
+    }
+
+    public String getCreateUser() {
+        return createUser;
+    }
+
+    public void setCreateUser(String createUser) {
+        this.createUser = createUser == null ? null : createUser.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getUpdateUser() {
+        return updateUser;
+    }
+
+    public void setUpdateUser(String updateUser) {
+        this.updateUser = updateUser == null ? null : updateUser.trim();
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
