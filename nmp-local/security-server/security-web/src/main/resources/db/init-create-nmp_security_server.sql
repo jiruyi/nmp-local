08183fb5291fc400bfad94ffc19b2eae54438df6
zhangyunjie <836507105@qq.com>
2023-11-14 11:05:54 +0800

    [安全服务器初始化sql]

--- nmp-local/security-server/security-web/src/main/resources/db/init-create-nmp_security_server.sql
+++ nmp-local/security-server/security-web/src/main/resources/db/init-create-nmp_security_server.sql
@@ -88,7 +88,7 @@ CREATE TABLE `nmps_data_info` (
            `network_id` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '入网id',
            `data_value` bigint NOT NULL COMMENT '数据值（单位byte）',
            `data_type` varchar(4) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '数据类型（1000：剩余上行密钥量 1001：已使用上行密钥量 2000：剩余下行密钥量 2001：已使用下行密钥量）',
-           `upload_time` datetime(2) DEFAULT CURRENT_TIMESTAMP(2) COMMENT '上报时间',
+           `upload_time` datetime(2) NOT NULL COMMENT '上报时间',
            `create_time` datetime(2) DEFAULT CURRENT_TIMESTAMP(2) COMMENT '创建时间',
            PRIMARY KEY (`id`),
            KEY `index_time` (`upload_time`)
