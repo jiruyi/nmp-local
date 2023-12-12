1233ee9d3d4b0f46e51df80f150557d40e8c2189
wangqiang <wq199269>
2023-12-12 10:16:19 +0800

    增量sql

--- nmp-local/nmp-local-proxy/proxy-web/src/main/resources/db/migration/V4.0.3_20231205_nmpl_proxy.sql
+++ nmp-local/nmp-local-proxy/proxy-web/src/main/resources/db/migration/V4.0.3_20231205_nmpl_proxy.sql
@@ -5,3 +5,8 @@ V4.0.3增量sql包
 USE `nmp_proxy`;
 
 update nmpl_link set active_auth = null,link_type = null where main_device_type <> '02';
+
+-- wq
+truncate table nmpl_company_heartbeat;
+
+truncate table nmpl_system_heartbeat;
